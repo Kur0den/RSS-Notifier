f67d5182a122aa61a1a5ccd02843cbc631386520
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-07-29 05:49:02 +0900

    作りたいものはできた

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import time
+
+import schedule
 from requests import get
 from xmltodict import parse
 
 
-rss_url = "RSS_URL"
-response = get(rss_url)
-print(type(response.text))
-parse_data = parse(response.text)
+def job():
+    rss_url = "RSS_URL" # RSSのURL
+    response = get(rss_url) # RSSを取得
+    parse_data = parse(response.text) # RSS(xml)をDict化
 
-'''
-あとはparse_dataから欲しい情報を引っ張り出して使う
+    '''
+    ここでparse_dataから欲しい情報を引っ張り出して使う
 
-使いにくいのはしらん'''
+    使いにくいのはしらん
+    '''
+
+## 分ごとにRSSを取得したいなら
+schedule.every(MINUTE).minutes.do(job)
+## 時ごとに取得なら
+# schedule.every(HOUR).hours.do(job)
+## 指定時刻に取得なら
+# schedule.every().day.at("HH:MM", "TIMEZONE").do(job)
+
+## 詳しくは https://schedule.readthedocs.io/en/stable/index.html
+
+# ずっと実行させるやつ(?)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
